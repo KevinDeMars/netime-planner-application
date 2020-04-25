3bd0a6d671ee718fc44c86f9b5342b82be8db843
Eric Jaroszewski <eric_jaroszewski1@baylor.edu>
2020-04-25 17:20:15 -0500

    Added in a getFreeTime() function into ScheduleService class, which takes the Schedule and a DateTimeInterval and returns the number of free hours present in that interval.

--- src/main/java/edu/baylor/csi3471/netime_planner/services/ScheduleService.java
+++ src/main/java/edu/baylor/csi3471/netime_planner/services/ScheduleService.java
@@ -1,12 +1,18 @@
 package edu.baylor.csi3471.netime_planner.services;
 
+import edu.baylor.csi3471.netime_planner.models.DateTimeInterval;
 import edu.baylor.csi3471.netime_planner.models.ScheduleEventListener;
+import edu.baylor.csi3471.netime_planner.models.domain_objects.Activity;
 import edu.baylor.csi3471.netime_planner.models.domain_objects.Event;
 import edu.baylor.csi3471.netime_planner.models.domain_objects.Schedule;
 import edu.baylor.csi3471.netime_planner.models.domain_objects.User;
 import edu.baylor.csi3471.netime_planner.models.persistence.ScheduleDAO;
 import edu.baylor.csi3471.netime_planner.models.persistence.UserDAO;
 
+import java.time.Duration;
+import java.time.Period;
+import java.time.chrono.ChronoLocalDate;
+import java.time.temporal.TemporalUnit;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -38,6 +44,42 @@ public class ScheduleService {
         var mgr = ServiceManager.getInstance();
         var user = mgr.getService(UserDAO.class).findByUsername(username);
         return user.map(User::getSchedule).orElse(null);
+    }
+
+    public long getFreeTime(Schedule s, DateTimeInterval d){
+
+
+        long hours = Duration.between(d.getStart(), d.getEnd()).toHours();
+        for (Activity a : s.getWorkTimes()){
+            if(a.getEndDate().isPresent()) {
+                if (d.contains(a.getStartDate().atTime(d.getStart().toLocalTime())) ||
+                        d.contains(a.getEndDate().get().atTime(d.getEnd().toLocalTime()))) {
+                    hours -= Duration.between(a.getStartDate(), a.getEndDate().get()).toHours();
+                    hours -= Duration.between(a.getTime().getStart(), a.getTime().getEnd()).toHours();
+
+                    //If the activity starts before the DateTimeInterval's starting point,
+                    //add back the excess time removed
+                    if (a.getStartDate().isBefore(ChronoLocalDate.from(d.getStart())) ||
+                            (a.getStartDate().equals(ChronoLocalDate.from(d.getStart()))
+                                    && a.getTime().getStart().isBefore(d.getStart().toLocalTime()))) {
+                        hours += Duration.between(a.getStartDate(), d.getStart().toLocalDate()).toHours();
+                        hours += Duration.between(a.getTime().getStart(), d.getStart().toLocalTime()).toHours();
+                    }
+
+                    //If the activity ends after the DateTimeInterval's end point,
+                    //add back the excess time removed
+                    if (a.getEndDate().get().isAfter(ChronoLocalDate.from(d.getEnd())) ||
+                            (a.getEndDate().get().equals(ChronoLocalDate.from(d.getEnd()))
+                                    && a.getTime().getEnd().isAfter(d.getEnd().toLocalTime()))) {
+                        hours += Duration.between(d.getEnd().toLocalDate(), a.getEndDate().get()).toHours();
+                        hours += Duration.between(d.getEnd().toLocalTime(), a.getTime().getEnd()).toHours();
+                    }
+                }
+            }
+        }
+        if(hours < 0)
+            hours = 0;
+        return hours;
     }
 
     public void listenToChanges(ScheduleEventListener l) {
