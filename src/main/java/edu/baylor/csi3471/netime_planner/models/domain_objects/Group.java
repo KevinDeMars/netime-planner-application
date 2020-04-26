8ffcb6e46e3eb3630621e967852fb1fa678f8867
Eric Jaroszewski <eric_jaroszewski1@baylor.edu>
2020-04-25 19:15:18 -0500

    Added in classes GroupScreen and GroupScreenController, added getter and setter functions to Group class

--- src/main/java/edu/baylor/csi3471/netime_planner/models/domain_objects/Group.java
+++ src/main/java/edu/baylor/csi3471/netime_planner/models/domain_objects/Group.java
@@ -14,4 +14,60 @@ public class Group extends DomainObject {
     private Collection<Event> sharedEvents;
 
     private Schedule schedule;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public Collection<User> getMembers() {
+        return members;
+    }
+
+    public void setMembers(Collection<User> members) {
+        this.members = members;
+    }
+
+    public GroupPermission getPermissions() {
+        return permissions;
+    }
+
+    public void setPermissions(GroupPermission permissions) {
+        this.permissions = permissions;
+    }
+
+    public Collection<Schedule> getSharedSchedules() {
+        return sharedSchedules;
+    }
+
+    public void setSharedSchedules(Collection<Schedule> sharedSchedules) {
+        this.sharedSchedules = sharedSchedules;
+    }
+
+    public Collection<Event> getSharedEvents() {
+        return sharedEvents;
+    }
+
+    public void setSharedEvents(Collection<Event> sharedEvents) {
+        this.sharedEvents = sharedEvents;
+    }
+
+    public Schedule getSchedule() {
+        return schedule;
+    }
+
+    public void setSchedule(Schedule schedule) {
+        this.schedule = schedule;
+    }
 }
